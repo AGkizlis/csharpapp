05eb280815976c7b3fa6e107433d48c1d57fafb9
Alexandros Gkizlis <agkizlis@gmail.com>
2024-08-10 15:11:06 +0300

    Changed TodoService to use injected HttpClient, removed Logger as it wasn't used, and some syntax changed

--- src/CSharpApp.Application/Services/TodoService.cs
+++ src/CSharpApp.Application/Services/TodoService.cs
@@ -1,33 +1,18 @@
 namespace CSharpApp.Application.Services;
 
-public class TodoService : ITodoService
+public sealed class TodoService(HttpClient httpClient) : ITodoService, IDisposable
 {
-    private readonly ILogger<TodoService> _logger;
-    private readonly HttpClient _client;
 
-    private readonly string? _baseUrl;
+	public async Task<TodoRecord?> GetTodoById(int id)
+	{
+		return await httpClient.GetFromJsonAsync<TodoRecord>($"todos/{id}");
+	}
 
-    public TodoService(ILogger<TodoService> logger, 
-        IConfiguration configuration)
-    {
-        _logger = logger;
-        _client = new HttpClient();
-        _baseUrl = configuration["BaseUrl"];
-    }
+	public async Task<ReadOnlyCollection<TodoRecord>> GetAllTodos()
+	{
+		var response = await httpClient.GetFromJsonAsync<List<TodoRecord>>($"todos");
+		return response!.AsReadOnly();
+	}
 
-    public async Task<TodoRecord?> GetTodoById(int id)
-    {
-        _client.BaseAddress = new Uri(_baseUrl!);
-        var response = await _client.GetFromJsonAsync<TodoRecord>($"todos/{id}");
-
-        return response;
-    }
-
-    public async Task<ReadOnlyCollection<TodoRecord>> GetAllTodos()
-    {
-        _client.BaseAddress = new Uri(_baseUrl!);
-        var response = await _client.GetFromJsonAsync<List<TodoRecord>>($"todos");
-
-        return response!.AsReadOnly();
-    }
+	public void Dispose() => httpClient?.Dispose();
 }
