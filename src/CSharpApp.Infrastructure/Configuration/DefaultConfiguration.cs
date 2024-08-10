5aafbb84cfa8df4cabe87eea74bf66c5a591ef44
Alexandros Gkizlis <agkizlis@gmail.com>
2024-08-10 20:00:16 +0300

    Added HttpClient for PostsService

--- src/CSharpApp.Infrastructure/Configuration/DefaultConfiguration.cs
+++ src/CSharpApp.Infrastructure/Configuration/DefaultConfiguration.cs
@@ -4,6 +4,7 @@ namespace CSharpApp.Infrastructure.Configuration;
 
 public static class DefaultConfiguration
 {
+	// ---- TodoService ----
 	public static IServiceCollection AddDefaultConfiguration(
 		this IServiceCollection services,
 		IConfiguration configuration)
@@ -26,6 +27,28 @@ public static class DefaultConfiguration
 				.GetRequiredService<IHttpClientFactory>()
 				.CreateClient(nameof(TodoService));
 			return ActivatorUtilities.CreateInstance<TodoService>(sp, httpClient);
+		});
+
+
+		// ---- PostsService ----
+		services.AddHttpClient<PostsService>(
+			client =>
+			{
+				string? baseUrl = configuration["BaseUrl"];
+				if (baseUrl == null)
+				{
+					throw new ArgumentException(nameof(baseUrl));
+				}
+				client.BaseAddress = new Uri(baseUrl);
+			});
+
+		// Use a factory method to create IPostsService using the configured PostsService
+		services.AddScoped<IPostsService>(sp =>
+		{
+			var httpClient = sp
+				.GetRequiredService<IHttpClientFactory>()
+				.CreateClient(nameof(PostsService));
+			return ActivatorUtilities.CreateInstance<PostsService>(sp, httpClient);
 		});
 
 		return services;
