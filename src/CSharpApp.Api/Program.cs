49308b6ec957860bdd30ea74ea36500cd2611eed
Alexandros Gkizlis <agkizlis@gmail.com>
2024-08-10 16:05:44 +0300

    Added DI for typed HttpClient, fixed DI for ITodoService so it's only registered once.

--- src/CSharpApp.Api/Program.cs
+++ src/CSharpApp.Api/Program.cs
@@ -5,7 +5,7 @@ builder.Host.UseSerilog(new LoggerConfiguration().ReadFrom.Configuration(builder
 builder.Configuration.AddEnvironmentVariables();
 
 // Add services to the container.
-builder.Services.AddDefaultConfiguration();
+builder.Services.AddDefaultConfiguration(builder.Configuration);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -16,26 +16,26 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+		app.UseSwagger();
+		app.UseSwaggerUI();
 }
 
 //app.UseHttpsRedirection();
 
-app.MapGet("/todos", async (ITodoService todoService) =>
-    {
-        var todos = await todoService.GetAllTodos();
-        return todos;
-    })
-    .WithName("GetTodos")
-    .WithOpenApi();
+app.MapGet("/todos", async ([FromServices] ITodoService todoService) =>
+		{
+				var todos = await todoService.GetAllTodos();
+				return todos;
+		})
+		.WithName("GetTodos")
+		.WithOpenApi();
 
-app.MapGet("/todos/{id}", async ([FromRoute] int id, ITodoService todoService) =>
-    {
-        var todos = await todoService.GetTodoById(id);
-        return todos;
-    })
-    .WithName("GetTodosById")
-    .WithOpenApi();
+app.MapGet("/todos/{id}", async ([FromRoute] int id, [FromServices] ITodoService todoService) =>
+		{
+				var todos = await todoService.GetTodoById(id);
+				return todos;
+		})
+		.WithName("GetTodosById")
+		.WithOpenApi();
 
 app.Run();
--- src/CSharpApp.Infrastructure/Configuration/DefaultConfiguration.cs
+++ src/CSharpApp.Infrastructure/Configuration/DefaultConfiguration.cs
@@ -1,11 +1,33 @@
+using Microsoft.Extensions.Configuration;
+
 namespace CSharpApp.Infrastructure.Configuration;
 
 public static class DefaultConfiguration
 {
-    public static IServiceCollection AddDefaultConfiguration(this IServiceCollection services)
-    {
-        services.AddScoped<ITodoService, TodoService>();
-        
-        return services;
-    }
+		public static IServiceCollection AddDefaultConfiguration(
+			this IServiceCollection services,
+			IConfiguration configuration)
+		{
+			services.AddHttpClient<TodoService>(
+				client =>
+				{
+					string? baseUrl = configuration["BaseUrl"];
+					if (baseUrl == null)
+					{
+						throw new ArgumentException(nameof(baseUrl));
+					}
+					client.BaseAddress = new Uri(baseUrl);
+				});
+
+			// Use a factory method to create ITodoService using the configured TodoService
+			services.AddScoped<ITodoService>(sp =>
+			{
+				var httpClient = sp
+					.GetRequiredService<IHttpClientFactory>()
+					.CreateClient(nameof(TodoService));
+				return ActivatorUtilities.CreateInstance<TodoService>(sp, httpClient);
+			});
+
+			return services;
+		}
 }
